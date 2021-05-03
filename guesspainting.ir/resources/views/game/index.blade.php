367ba31ceb07f4b0492e66cea034f2f3846e24d5
mehrshadk05 <mehrshadk2005@gmail.com>
2021-05-03 18:58:03 +0430

    jigar

--- guesspainting.ir/resources/views/game/index.blade.php
+++ guesspainting.ir/resources/views/game/index.blade.php
@@ -1,6 +1,5 @@
- <!-- <div class="row">
-     <div class="col-md-2" style="text-align: center; border: 1px solid #212121;">@include('game.playerslist.index')</div>
-     <div class="col-md-7" style="text-align: center; border: 1px solid #212121;">@include('game.whiteboard.index')</div>
-     <div class="col-md-3" style="text-align: center;">@include('game.chatbox.index')</div>
- </div> -->
- <div style="text-align: center;">@include('game.chatbox.index')</div>
+<div class="row">
+    <div class="col-md-2" style="text-align: center; border: 1px solid #212121;">@include('game.playerslist.index')</div>
+    <div class="col-md-7" style="text-align: center; border: 1px solid #212121;">@include('game.whiteboard.index')</div>
+    <div class="col-md-3" style="text-align: center;">@include('game.chatbox.index')</div>
+</div>
